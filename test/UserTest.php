a32fbd4cbaacad4cb8be58b3cc9f99ac977ce46f
Taukon <83957880+Taukon@users.noreply.github.com>
2022-08-24 21:02:31 +0900

    #11 Userのテスト

---
+++ test/UserTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use Taukon\TodoApp\Classes\User;
+
+final class UserTest extends TestCase{
+
+    public function test作成(){   
+       
+        $this->assertEquals(1,1);
+    }
+}
