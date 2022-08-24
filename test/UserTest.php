a3f03dad05123b052529b30c3898d55db652d22c
Taukon <83957880+Taukon@users.noreply.github.com>
2022-08-25 02:40:47 +0900

    #11 loginのテスト

--- test/UserTest.php
+++ test/UserTest.php
@@ -1,4 +1,6 @@
 <?php declare(strict_types=1);
+ob_start();
+session_start();
 use PHPUnit\Framework\TestCase;
 use Taukon\TodoApp\Classes\User;
 
@@ -10,5 +12,23 @@ final class UserTest extends TestCase{
 
     public function testCheckUserByName失敗(){
         $this->assertSame(false, User::checkUserByName('php_test'));
+    }
+
+    public function testLogin成功(){
+        $this->assertSame(true, User::login('testuser','testuser'));
+    }
+
+    public function testLogin失敗(){
+        $this->assertSame(false, User::login('testuser','testtesttest'));
+    }
+
+    protected function tearDown(): void
+    {
+        $_SESSION = array();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        session_destroy();
     }
 }
